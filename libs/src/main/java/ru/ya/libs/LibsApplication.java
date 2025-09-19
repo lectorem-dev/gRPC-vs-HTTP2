ed71eb14c698a90c2d88038da5053ac5027ccfcc
lectorem <79637710149@mail.ru>
2025-09-19 14:55:02 +0300

    инициализировал проект

---
+++ libs/src/main/java/ru/ya/libs/LibsApplication.java
@@ -0,0 +1,13 @@
+package ru.ya.libs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibsApplication.class, args);
+    }
+
+}
---
+++ libs/src/test/java/ru/ya/libs/LibsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ya.libs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
